21bbbfe016e1e1d5ae926e884378b6c3f9a3f17a
jariryyousef <jariryyousef@gmail.com>
2021-09-07 23:31:45 +0300

    try

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const server = express(); 
 
 require("dotenv").config();
+
 const cors = require("cors");
 
 server.use(cors()); 
@@ -10,33 +11,48 @@ const axios = require("axios");
 
 const PORT = process.env.PORT; //
 
+const WATHER_API_KEY = process.env.WATHER_API_KEY;
+
+
 // a server endpoint
 server.get(
   "/", // our endpoint name
   function (req, res) {
     // callback function of what we should do with our request
     res.send("Hello World"); // our endpoint function response
+    
   }
 );
 
 // to hold data file from json file
-const weatherData = require("./weather.json"); //
+const weatherData = require("./weather.json"); 
 
 server.get(
   "/weather",
 
-  (request, response) => {
+  async (request, response) => {
 
     class Forecast {
       constructor (date,description){
-    this.date=date;
-    this.description=description;
+    this.date=date.valid_date;
+    this.description=description.weather.description;
     }}
     
     console.log(request.query);
     const nameOfCity = request.query.searchQ;
-    const lat = request.query.lat;
-    const lon = request.query.lon;
+    
+    // const lat = request.query.lat;
+    // const lon = request.query.lon;
+
+
+    const watherBitUrl='https://api.weatherbit.io/v2.0/forecast/daily?';
+
+    const watherBitResponse= await axios.get(`${watherBitUrl}?city=${nameOfCity}&key=${WATHER_API_KEY}`);
+
+
+    response.json(watherBitResponse.data);
+    
+        
 
     const result = weatherData.find((item) => {
       return (item.city_name.toLowerCase() === nameOfCity.toLowerCase());
@@ -46,25 +62,22 @@ server.get(
     if (result){
       let newArr =result.data.map((item) => {
 
-        return new Forecast (item.datetime,item.weather.description)
-
-
+        return new Forecast (item.valid_date,item.weather.description)
       })
       response.json(newArr);
-
-
     }
     else{
       response.json('data not found')
   }
     
-
-  }
+  console.log(watherBitResponse);
+  
+}
 );
 
-// server.listen(3001, ()=> {
-//     console.log("testttaat");}) // kick start the express server to work
+server.listen(3001, ()=> {
+    console.log("testttaat");}) // kick start the express server to work
 
-server.listen(PORT, () => {
-  console.log(`'port' ${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`'port' ${PORT}`);
+// });
