85ce302558ba766253e8d07f5bf64b0ffceaefc5
jariryyousef <jariryyousef@gmail.com>
2021-09-07 15:46:37 +0300

    finsh

--- server.js
+++ server.js
@@ -1,64 +1,70 @@
-const express = require('express') // require the express package
-const server = express() // initialize your express app instance
- 
+const express = require("express"); 
+const server = express(); 
 
-require('dotenv').config();
-const cors = require('cors');
+require("dotenv").config();
+const cors = require("cors");
 
-server.use(cors()) // after you initialize your express app instance
+server.use(cors()); 
 
-const axios = require('axios'); // require the package
+const axios = require("axios"); 
 
-// a server endpoint 
-server.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
-  
-})
- 
+const PORT = process.env.PORT; //
+
+// a server endpoint
+server.get(
+  "/", // our endpoint name
+  function (req, res) {
+    // callback function of what we should do with our request
+    res.send("Hello World"); // our endpoint function response
+  }
+);
+
 // to hold data file from json file
-const weatherData=require('./weather.json');
-const { response } = require('express');
-server.get('/weather',
+const weatherData = require("./weather.json"); //
 
-(request, response) => {
-    // const city_name=request.query.city_name;
+server.get(
+  "/weather",
 
-    const nameOfCity= request.query.searchQ;
-    const lat= request.query.lat;
-    const lon =request.query.lon;
+  (request, response) => {
 
+    class Forecast {
+      constructor (date,description){
+    this.date=date;
+    this.description=description;
+    }}
+    
+    console.log(request.query);
+    const nameOfCity = request.query.searchQ;
+    const lat = request.query.lat;
+    const lon = request.query.lon;
 
-    const result = weatherData.find((item) =>{
+    const result = weatherData.find((item) => {
+      return (item.city_name.toLowerCase() === nameOfCity.toLowerCase());
+    });
 
-        if(item.city_name === nameOfCity && item.lat === lat && item.lon === lon ){
-          return item;
-        }
-        }
-        )
+    
+    if (result){
+      let newArr =result.data.map((item) => {
 
-        // response.send(result);
-// })
-
-
-let data = result.data.map(item=>{
-    return new Forcast(item);
-  })
-
-  response.send(data);
-
-// catch{
-//   res.send('404 Not Found');
-// }
-})
-
-class Forcast {
-constructor(item){
-  this.date = item.valid_date;
-  this. description= item.weather.description ;
-}
-}
+        return new Forecast (item.datetime,item.weather.description)
 
 
-server.listen(3001, ()=> {
-    console.log("testttaat");}) // kick start the express server to work
+      })
+      response.json(newArr);
+
+
+    }
+    else{
+      response.json('data not found')
+  }
+    
+
+  }
+);
+
+// server.listen(3001, ()=> {
+//     console.log("testttaat");}) // kick start the express server to work
+
+server.listen(PORT, () => {
+  console.log(`'port' ${PORT}`);
+});
