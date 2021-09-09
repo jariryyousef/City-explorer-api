68b762158d6aa75283e3dac8b8f362948acaf65a
jariryyousef <jariryyousef@gmail.com>
2021-09-09 21:59:42 +0300

    finsh

---
+++ models/movie.model.js
@@ -0,0 +1,15 @@
+'use strict';
+
+class Movie {
+    constructor(item) {
+      this.title = item.title;
+      this.overview = item.overview;
+      this.average_votes = item.vote_average;
+      this.total_votes = item.vote_count;
+      this.image_url = `https://image.tmdb.org/t/p/w500${item.backdrop_path}`;
+      this.popularity = item.popularity;
+      this.released_on = item.release_date;
+    }
+  }
+
+  module.exports = Movie;
