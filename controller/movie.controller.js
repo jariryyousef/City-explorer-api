0f23f2a78705a4a789bf5b6791c702e036442180
jariryyousef <jariryyousef@gmail.com>
2021-09-10 23:04:33 +0300

    finish

--- controller/movie.controller.js
+++ controller/movie.controller.js
@@ -1,21 +1,27 @@
 const axios = require("axios"); 
+const { query } = require("express");
 require('dotenv').config();
 
 const Movie = require('../models/movie.model');
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
-
+const myMemory ={};
 
 const getMovie= async (request, response) => {
     const nameOfCity1 = request.query.searchQ;
-  
+  if (myMemory[query]!==undefined){
+    response.json(myMemory[query]);
+  }else{
+
     const moviesResponse = await axios.get(
       `https://api.themoviedb.org/3/search/movie?&api_key=${MOVIE_API_KEY}&query=${nameOfCity1}`
     );
-  
     const newMovie = moviesResponse.data.results.map((item) => {
       return new Movie(item);
     });
+    myMemory[query]=newMovie;
     response.json(newMovie);
+  }
+
   };
 
   module.exports = getMovie;
